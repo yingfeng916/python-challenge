b9f6ee0d8a44280a5f68b195ef238b67c024d83c
Ying Feng <yingfeng@YF-Macbook.local>
2020-06-19 22:11:15 -0400

    PyBank version 1

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,9 +1,33 @@
 import os
 import csv
 
+total_months = 0
+previous_value = 0
+total_profit_loss = []
+average_change = []
+
+
+
+
 csvpath = os.path.join("Resources", "data.csv")
 with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
-    print(csvreader)
+    next(csvreader)
+
+    for line in csvreader:
+        total_months += 1
+        total_profit_loss.append(int(line[1]))
+        if previous_value != 0:
+            average_change.append(int(line[1]) - previous_value)
+        previous_value = int(line[1])
+        
+        
     
-    
+       
+    print("Financial Analysis")
+    print("------------------------")
+    print("Total Months: " + str(total_months))
+    print("Total: " + "$" + str(sum(total_profit_loss)))
+    print("Average Change: " + "$" + str(round(sum(average_change)/len(average_change), 2)))
+    print("Greatest Increase in Profits: " + "$" + str(max(average_change)))
+    print("Greatest Decrease in Profits: " + "$" + str(min(average_change)))
